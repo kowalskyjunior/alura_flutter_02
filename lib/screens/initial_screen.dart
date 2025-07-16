003c6cd48c666c0e6ff3612ea19283937a68920a
Kowalsky Júnior <kowalskymesquita.jr@gmail.com>
2025-07-16 15:35:18 -0300

    Initial commit

---
+++ lib/screens/initial_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '/data/task_inherited.dart';
+import '/screens/form_screen.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({super.key});
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        title: const Text('Tarefas', style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView(
+        children: TaskInherited.of(context).taskList,
+        padding: EdgeInsets.only(top: 8, bottom: 70),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (contextNew) => FormScreen(taskContext: context,),
+            ),
+          );
+        },
+        child:
+        const Icon(Icons.add),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
+    );
+  }
+}
